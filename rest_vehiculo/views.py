b70cff8cf2a7425c088bdd67d322b8a0c527174d
Alain Maltrain <acmvaldivieso@gmail.com>
2021-06-29 22:04:50 -0400

    API REST POST Y GET

--- rest_vehiculo/views.py
+++ rest_vehiculo/views.py
@@ -23,3 +23,28 @@ def lista_vehiculos(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET','PUT','DELETE'])
+def detalle_vehiculo(request,id):
+    try:
+        vehiculo = Vehiculo.objects.get(patente=id)
+    except Vehiculo.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'GET':
+        serializer = VehiculoSerializer(vehiculo)
+        return Response(serializer.data)
+    if request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = VehiculoSerializer(vehiculo,data=data)
+        if(serializer.is_valid()):
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        vehiculo.delete()
+        return Response(status=status.HTTP_204_NOT_CONTENT)
+
+
+
+
